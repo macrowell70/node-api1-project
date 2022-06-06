fb869f1ec13ccdb906a4b4f8b76c8459f9c6dde1
unknown <macrowell@yahoo.com>
2022-06-06 18:59:17 -0400

    added post request to server that functions properly

--- api/server.js
+++ api/server.js
@@ -6,6 +6,18 @@ const server = express();
 
 server.use(express.json());
 
+server.post('/api/users', (req, res) => {
+    Users.insert(req.body)
+    .then(result => {
+        if (!req.body.name || !req.body.bio) {
+            res.status(400).json({ message: "Please provide name and bio for the user" });
+            return;
+        }
+        res.status(201).json(result);
+    })
+    .catch(result => res.status(500).json({ message: "There was an error while saving the user to the database" }));
+});
+
 server.get('/api/users', (req, res) => {
     Users.find().then(result => {
         if (result == null) {

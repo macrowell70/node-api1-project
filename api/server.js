0c54e8d88083c2bd0f96707de80d1fcd1f8d2f03
unknown <macrowell@yahoo.com>
2022-06-06 18:17:51 -0400

    server is listening to the correct port

--- api/server.js
+++ api/server.js
@@ -1,3 +1,9 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const users = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}

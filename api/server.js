2dbe7486511611d00149e01aad22bed2639fd28d
unknown <macrowell@yahoo.com>
2022-06-06 18:38:35 -0400

    set up get requests to fetch all users

--- api/server.js
+++ api/server.js
@@ -1,9 +1,19 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
-const users = require('./users/model');
+const Users = require('./users/model');
 
 const server = express();
 
+server.use(express.json());
 
+server.get('/api/users', (req, res) => {
+    Users.find().then(result => {
+        if (result == null) {
+            res.status(500).json({ message: "the user information could not be retrieved" });
+            return;
+        }
+        res.json(result)
+    });
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
